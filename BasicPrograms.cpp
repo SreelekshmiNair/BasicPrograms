56fd6c2e8122131d47443606854b663e1bfe5fe1
SreelekshmiNair <sreelekshmisnair.92@gmail.com>
2021-07-08 11:29:18 +0530

    New functions added

--- BasicPrograms.cpp
+++ BasicPrograms.cpp
@@ -1,8 +1,10 @@
-// BasicPrograms.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// BasicPrograms.cpp : Sreelekshmi S Amma
+// Description : This program consists of several Basic programs that can be done in C++ organised as a menu.
 //
 
 #include <iostream>
 #include<math.h>
+#include<string.h>
 // #include<stdlib.h>
 using namespace std;
 
@@ -15,6 +17,14 @@ void DistanceTravelled();
 void SimpleInterest();
 void VolumeofCylinder();
 void DistancebtwPoints();
+void SwapNumbers();
+void SizeDatatypes();
+void FindASCII();
+void Factorial();
+void BinaryToDecimal();
+void FreqOfChar();
+void strngcomp1();
+void strngcomp2();
 
 int main()
 {
@@ -31,6 +41,14 @@ int main()
         cout << "  6 : Find the Simple Interest" << endl;
         cout << "  7 : Find the volume of a Cylinder" << endl;
         cout << "  8 : Find the distance between two 2-d points" << endl;
+        cout << "  9 : Swap 2 numbers without using 3rd variable" << endl;
+        cout << " 10 : Find the Size of Datatypes" << endl;
+        cout << " 11 : Find the ASCII value of a character" << endl;
+        cout << " 12 : Find the Factorial of a number " << endl;
+        cout << " 13 : Convert the binary to decimal " << endl;
+        cout << " 14 : Find the frequency of occurance of a character in a string " << endl;
+        cout << " 15 : Compare 2 strings using builtin function : " << endl;
+        cout << " 16 : Compare 2 strings without strcmp() " << endl;
 //Last option for exiting
         cout << "100 : Exit the Program" << endl;
         cout << "Select the required action by their number : " << endl;
@@ -65,6 +83,30 @@ int main()
             break;
         case 8:
             DistancebtwPoints();
+            break;
+        case 9:
+            SwapNumbers();
+            break;
+        case 10:
+            SizeDatatypes();
+            break;
+        case 11:
+            FindASCII();
+            break;
+        case 12:
+            Factorial();
+            break;
+        case 13:
+            BinaryToDecimal();
+            break;
+        case 14:
+            FreqOfChar();
+            break;
+        case 15:
+            strngcomp1();
+            break;
+        case 16:
+            strngcomp2();
             break;
         case 100:
            abort();
@@ -136,11 +178,11 @@ void DistanceTravelled()
    if(val==false){abort();}
     cout << "Enter the final velocity in m/s : " << endl;
     cin >> v;
-    bool val = ValidateInput();
+    bool val1 = ValidateInput();
    if(val==false){abort();}
     cout << "Enter the rate of acceleration in m/sec2 : " << endl;
     cin >> a;
-    bool val = ValidateInput();
+    bool val2 = ValidateInput();
    if(val==false){abort();}
     S = (pow(v, 2) - pow(u, 2)) / (2 * a);
     cout << "The distance travelled is " << S << "m"<<endl;
@@ -156,11 +198,11 @@ void SimpleInterest()
    if(val==false){abort();}
     cout << "Enter the Number of Years : " << endl;
     cin >> N;
-    bool val = ValidateInput();
+    bool val1 = ValidateInput();
    if(val==false){abort();}
     cout << "Enter the Rate of Interest : " << endl;
     cin >> R;
-    bool val = ValidateInput();
+    bool val2 = ValidateInput();
    if(val==false){abort();}
     SI = (float)P * N * R / 100;
     cout << "The Simple interest is " << SI << endl;
@@ -176,7 +218,7 @@ void VolumeofCylinder()
    if(val==false){abort();}
     cout << "Enter the height of the Cylinder : " << endl;
     cin >> h;
-    bool val = ValidateInput();
+    bool val1 = ValidateInput();
    if(val==false){abort();}
     volume = 3.14 * rad * rad * h;
     cout << " Volume of Cylinder is : " << volume << endl;
@@ -192,9 +234,104 @@ void DistancebtwPoints()
    if(val==false){abort();}
     cout << "Enter the (x,y) of last point : " << endl;
     cin >> x2 >> y2;
-    bool val = ValidateInput();
+    bool val1 = ValidateInput();
    if(val==false){abort();}
     dis = sqrt((pow(x2 - x1, 2)) - (pow(y2 - y1, 2)));
     cout << "Distance between 2 points is " << dis;
     cout << "********************************" << endl;
+}
+void SwapNumbers()
+{
+int num1,num2;
+cout << "Enter the first number : " <<endl;
+cin >> num1;
+ bool val = ValidateInput();
+   if(val==false){abort();}
+cout << "Enter the second number : " <<endl;
+cin >> num2;
+ bool val1 = ValidateInput();
+   if(val==false){abort();}
+cout << "Before swapping numbers are " << num1 << " and " << num2 << endl;
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+cout << "After swapping numbers are " << num1 << " and " << num2 << endl;
+}
+void SizeDatatypes()
+{
+cout << "Size of char data type is " << sizeof(char) << "bytes" << endl;
+cout << "Size of int data type is " << sizeof(int) << "bytes" << endl;
+cout << "Size of float data type is " << sizeof(float) << "bytes" << endl;
+cout << "Size of double data type is " << sizeof(double) << "bytes" << endl;
+}
+void FindASCII()
+{
+    char charac;
+cout<< "Enter a character : "<<endl;
+cin>>charac;
+cout<< "ASCII value of " << charac << " is " << int(charac) << endl;
+}
+void Factorial()
+{ 
+    int number,fact=1;
+cout << "Enter a positive number :" <<endl;
+cin >> number;
+bool val = ValidateInput();
+if(val==false){abort();}
+for(int i=2;i<=number;i++){
+fact = fact*i;
+}
+cout << "The Factorial of " << number << " is " << fact << endl;;
+}
+void BinaryToDecimal(){
+    int input,dec=0;
+    cout << "Enter the binary value : " <<endl;
+    cin >> input;
+     cout << "The decimal value of " << input << " is ";
+  for(int i=0;input!=0;++i)
+  {
+      int rem = input%10;
+      dec = dec+(rem * (pow(2,i)));
+      input = input/10;
+  }
+  cout << dec<<endl;
+}
+void FreqOfChar()
+{
+    int count = 0;
+    char str[50], ch;
+    cout << "Enter the string : " << endl;
+    cin >> str;
+    cout << "Enter the character to be searched : " << endl;
+    cin >> ch;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]==ch){count++;}
+    }
+if(count==0){cout<<"The character " << ch << " not found in the string " << str << endl;}
+   else{cout<<"The character " << ch << " found " << count << " times in the string " << str << endl;}
+}
+void strngcomp1(){
+    char str1[50], str2[50];
+    cout << "Enter the first string : " << endl;
+    cin >> str1;
+    cout << "Enter the second string : " << endl;
+    cin >> str2;
+    if(strcmp(str1,str2)==0){
+        cout << "Strings " << str1 << " and " << str2 << " are similar." <<endl;}
+    else{
+        cout << "Strings " << str1 << " and " << str2 << " are not similar." <<endl;}    
+}
+void strngcomp2(){
+    char str1[50], str2[50];int flag=0;
+    cout << "Enter the first string : " << endl;
+    cin >> str1;
+    cout << "Enter the second string : " << endl;
+    cin >> str2;
+    if(strlen(str1)==strlen(str2)){
+        for(int i=0;str1[i]!='\0',str2[i]!='\0';i++){if(str1[i]!=str2[i]){flag=1;break;}}
+    if(flag==0){cout << "Strings " << str1 << " and " << str2 << " are similar." <<endl;}
+    else{cout << "Strings " << str1 << " and " << str2 << " are not similar." <<endl;} 
+    }
+    else{
+        cout << "Strings " << str1 << " and " << str2 << " are not similar." <<endl;}    
 }
---
+++ Test.cpp
@@ -0,0 +1,13 @@
+
+#include<iostream>
+#include<string.h>
+int main()
+{
+    int a,b;
+    std::cout << "Enter first number : " << std::endl;
+    std::cin >> a;
+    std::cout << "Enter second number : "<<std::endl;
+    std::cin >> b;
+    int out = a+b;
+    std::cout << "The sum is " << out << std::endl;
+}
